a2175f95f0cc956fb129bf146d4537257fc85e92
Veronika943 <nice290307@gmail.com>
2026-03-11 11:20:07 +0300

    ТЕМА 1

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(306, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(114, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите точность 0<e<1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(506, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите X";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.Рисунок1;
+            this.pictureBox1.Location = new System.Drawing.Point(117, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(549, 103);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(145, 167);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(499, 167);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(95, 294);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Enabled = false; // Кнопка заблокирована изначально
+            label3.Text = ""; // Очищаем label с результатами
+            this.Text = "Вычисление функции разложением в ряд (Вариант 6)";
+        }
+
+        /// <summary>
+        /// Значение математической функции (левая часть)
+        /// ln((x+1)/(x-1))
+        /// </summary>
+        private double MathFunction(double x)
+        {
+            return Math.Log((x + 1) / (x - 1));
+        }
+
+        /// <summary>
+        /// Вычисление суммы ряда (правая часть)
+        /// 2 * ∑(1/((2n+1) * x^(2n+1))), n от 0 до ∞
+        /// </summary>
+        private double SeriesSum(double x, double eps, out int count)
+        {
+            // Первый член ряда при n=0: 2 * (1/x)
+            double a = 2.0 / x;        // первый член (n=0)
+            double sum = a;             // сумма начинается с первого члена
+            int n = 0;                   // номер члена (начинаем с 0)
+            count = 1;                   // количество просуммированных членов
+
+            // Рекуррентная формула: a(n+1) = a(n) * (2n+1)/(2n+3) * 1/x²
+            // Выводится из отношения (n+1)-го члена к n-му
+            while (Math.Abs(a) >= eps * Math.Abs(sum) && count < 1000000)
+            {
+                // Вычисляем следующий член через предыдущий
+                // a(n+1) = a(n) * (2n+1)/(2n+3) * 1/x²
+                a = a * (2 * n + 1) / (2 * n + 3) / (x * x);
+                sum += a;
+                count++;
+                n++;
+            }
+
+            return sum;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Чтение данных из полей ввода
+                double x = Convert.ToDouble(textBox2.Text);
+                double eps = Convert.ToDouble(textBox1.Text);
+
+                // Проверка области допустимых значений
+                // Для функции ln((x+1)/(x-1)): |x| > 1
+                if (Math.Abs(x) <= 1)
+                {
+                    label3.Text = "Ошибка: |x| должен быть > 1";
+                    return;
+                }
+
+                // Проверка точности: 0 < eps < 1
+                if (eps <= 0 || eps >= 1)
+                {
+                    label3.Text = "Ошибка: точность должна быть 0 < eps < 1";
+                    return;
+                }
+
+                // Вычисление значений
+                double mathResult = MathFunction(x);
+                int count;
+                double seriesResult = SeriesSum(x, eps, out count);
+
+                // Вывод результатов
+                label3.Text = $"Математическая функция ln(({x}+1)/({x}-1)) = {mathResult:F10}\n" +
+                              $"Сумма ряда = {seriesResult:F10}\n" +
+                              $"Количество просуммированных членов = {count}\n" +
+                              $"Погрешность = {Math.Abs(mathResult - seriesResult):E2}";
+            }
+            catch (FormatException)
+            {
+                label3.Text = "Ошибка: некорректный формат числа. Используйте запятую для дробной части.";
+            }
+            catch (DivideByZeroException)
+            {
+                label3.Text = "Ошибка: деление на ноль. Проверьте значение x.";
+            }
+            catch (Exception ex)
+            {
+                label3.Text = $"Ошибка: {ex.Message}";
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Разрешаем: цифры, backspace, запятую (для дробной части)
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != ',')
+            {
+                e.Handled = true; // Запрещаем ввод
+            }
+
+            // Разрешаем только одну запятую
+            if (e.KeyChar == ',' && (sender as TextBox).Text.Contains(','))
+            {
+                e.Handled = true;
+            }
+
+            // Для точности не разрешаем минус (точность всегда положительная)
+            if (e.KeyChar == '-')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Разрешаем: цифры, backspace, запятую, минус
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                e.KeyChar != ',' && e.KeyChar != '-')
+            {
+                e.Handled = true;
+            }
+
+            // Разрешаем только одну запятую
+            if (e.KeyChar == ',' && (sender as TextBox).Text.Contains(','))
+            {
+                e.Handled = true;
+            }
+
+            // Минус только в начале и только один
+            if (e.KeyChar == '-' && ((sender as TextBox).Text.Length > 0 ||
+                (sender as TextBox).SelectionStart > 0))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            CheckFields();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            CheckFields();
+        }
+        private void CheckFields()
+        {
+            // Проверяем, что поля не пустые и не содержат только служебные символы
+            bool isValid1 = !string.IsNullOrWhiteSpace(textBox1.Text) &&
+                           textBox1.Text != "," && textBox1.Text != "-";
+
+            bool isValid2 = !string.IsNullOrWhiteSpace(textBox2.Text) &&
+                           textBox2.Text != "-" && textBox2.Text != "," &&
+                           textBox2.Text != "-," && textBox2.Text != ",-";
+
+            button1.Enabled = isValid1 && isValid2;
+        }
+    }
+}

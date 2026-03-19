e5f92e6af1cc58828af04992ad18efc84386a955
unknown <232909071094-6>
2026-03-19 12:41:02 +0300

    a

--- WindowsFormsApp2/TestProject1/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ WindowsFormsApp2/TestProject1/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce460ad1237cd3ed8f1be4d5c2ab4b4cf0807a3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
